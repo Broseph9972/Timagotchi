afb7f5bf4de57a2822508efea93a667f86f15a80
broseph9972 <Josephaidenstein@gmail.com>
2025-12-14 09:41:28 -0500

    reduce debounce time for button presses and optimize button polling

--- Code/input_handler.py
+++ Code/input_handler.py
@@ -43,47 +43,65 @@ class InputHandler:
             self.last_press[pin] = 0
             self.last_state[pin] = 1  # 1 = not pressed (pull-up)
         
-        # Debounce time in seconds
-        self.debounce_time = 0.05
+        # Debounce time in seconds - keep very short for fast taps
+        self.debounce_time = 0.03
+        
+        # Queue for buffered presses
+        self.press_queue = []
+    
+    def _poll_all_buttons(self):
+        """Poll all buttons and queue any detected presses."""
+        current_time = time.time()
+        
+        for pin in self.pins:
+            if self.using_rpi_gpio:
+                button_state = GPIO.input(pin)
+            else:
+                button_state = lgpio.gpio_read(self.gpio_handle, pin)
+            
+            prev_state = self.last_state.get(pin, 1)
+            
+            # Detect falling edge: was not pressed (1), now pressed (0)
+            if prev_state == 1 and button_state == 0:
+                self.last_state[pin] = 0
+                if current_time - self.last_press[pin] > self.debounce_time:
+                    self.last_press[pin] = current_time
+                    self.press_queue.append(pin)
+            elif button_state == 1:
+                # Released
+                self.last_state[pin] = 1
     
     def check_button(self, pin):
-        if self.using_rpi_gpio:
-            button_state = GPIO.input(pin)
-        else:
-            button_state = lgpio.gpio_read(self.gpio_handle, pin)
-        
-        current_time = time.time()
-        prev_state = self.last_state.get(pin, 1)
-        
-        # Detect falling edge: was not pressed (1), now pressed (0)
-        if prev_state == 1 and button_state == 0:
-            self.last_state[pin] = 0
-            if current_time - self.last_press[pin] > self.debounce_time:
-                self.last_press[pin] = current_time
-                return True
-        elif button_state == 1:
-            # Released
-            self.last_state[pin] = 1
-        
+        # Legacy method - just poll and check
+        self._poll_all_buttons()
+        if pin in self.press_queue:
+            self.press_queue.remove(pin)
+            return True
         return False
     
     def get_input(self):
-        if self.check_button(self.KEY_UP_PIN):
-            return 'up'
-        elif self.check_button(self.KEY_DOWN_PIN):
-            return 'down'
-        elif self.check_button(self.KEY_LEFT_PIN):
-            return 'left'
-        elif self.check_button(self.KEY_RIGHT_PIN):
-            return 'right'
-        elif self.check_button(self.KEY_PRESS_PIN):
-            return 'select'
-        elif self.check_button(self.KEY1_PIN):
-            return 'key1'
-        elif self.check_button(self.KEY2_PIN):
-            return 'key2'
-        elif self.check_button(self.KEY3_PIN):
-            return 'key3'
+        # Poll all buttons first
+        self._poll_all_buttons()
+        
+        # Return first queued press
+        if self.press_queue:
+            pin = self.press_queue.pop(0)
+            if pin == self.KEY_UP_PIN:
+                return 'up'
+            elif pin == self.KEY_DOWN_PIN:
+                return 'down'
+            elif pin == self.KEY_LEFT_PIN:
+                return 'left'
+            elif pin == self.KEY_RIGHT_PIN:
+                return 'right'
+            elif pin == self.KEY_PRESS_PIN:
+                return 'select'
+            elif pin == self.KEY1_PIN:
+                return 'key1'
+            elif pin == self.KEY2_PIN:
+                return 'key2'
+            elif pin == self.KEY3_PIN:
+                return 'key3'
         return None
     
     def cleanup(self):
--- Code/menu.py
+++ Code/menu.py
@@ -1388,6 +1388,6 @@ class Menu:
                     self.sync_time_via_wifi()
                     self.last_sync_time = current_time
             
-            time.sleep(0.01)
+            time.sleep(0.005)
         
         self.input_handler.cleanup()
